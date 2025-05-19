0d83382d695fcddd2632934eee856637d0e75d87
TerrellAW <terraw1999@proton.me>
2025-05-18 22:12:48 -0600

    Add: Link and tags fields to Project

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,7 @@
 class Project < ApplicationRecord
     validates :name, presence: true # Ensure name is present
-    validates :desc, presence: true # Ensure description is present
+    validates :thumbnail_desc, presence: true # Ensure project card description is present
+    validates :desc, presence: true # Ensure full description is present
     validates :link, format: { with: URI::DEFAULT_PARSER.make_regexp, message: "is not a valid URL" } # Link to project page
     validates :img_1_desc, presence: true # Text under first image
     validates :img_2_desc, presence: true # Text under second image
@@ -9,4 +10,5 @@ class Project < ApplicationRecord
     has_one_attached :img_1 # First image on page
     has_one_attached :img_2 # Second image on page
     has_one_attached :img_3 # Third image on page
+    has_and_belongs_to_many :tags # Tags for project cards
 end
