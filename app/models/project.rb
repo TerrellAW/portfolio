53e4dbc9db6d7638e0e69c14c5f7fbb9a0f1728d
TerrellAW <terraw1999@proton.me>
2025-06-01 22:07:14 -0600

    Feature: Added project page template

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,7 @@ class Project < ApplicationRecord
     validates :name, presence: true # Ensure name is present
     validates :thumbnail_desc, presence: true # Ensure project card description is present
     validates :desc, presence: true # Ensure full description is present
-    validates :link, format: { with: URI::DEFAULT_PARSER.make_regexp, message: "is not a valid URL" } # Link to project page
+    validates :link, format: { with: URI::DEFAULT_PARSER.make_regexp, message: "is not a valid URL" }, allow_blank: true # Link to project page
     validates :github_link, format: { with: URI::DEFAULT_PARSER.make_regexp, message: "is not a valid URL" }, allow_blank: true # GitHub repository link
     validates :img_1_desc, presence: true # Text under first image
     validates :img_2_desc, presence: true # Text under second image
