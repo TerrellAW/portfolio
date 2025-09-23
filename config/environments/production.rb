90a756d2c3bf36ef3bca3da7780581feb88f86a2
TerrellAW <terraw1999@proton.me>
2025-09-23 17:20:15 -0600

    Fix: Production config allows web access

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,11 +80,11 @@ Rails.application.configure do
   config.active_record.attributes_for_inspect = [ :id ]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  # config.hosts = [
-  #   "example.com",     # Allow requests from example.com
-  #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
-  # ]
-  #
+  config.hosts = [
+    "terrellportfolio.duckdns.org",      # Allow requests from portfolio website domain
+    /.*\.terrellportfolio\.duckdns\.org/ # Allow requests from subdomains like `www.terrellportfolio.duckdns.org`
+  ]
+
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
