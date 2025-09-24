7100f6a3945c0b2bb537081a713bad3a9a3b99e2
TerrellAW <terraw1999@proton.me>
2025-09-24 17:18:52 -0600

    Fix: Finally fixed script

--- script/start.sh
+++ script/start.sh
@@ -18,24 +18,24 @@ docker stop portfolio-app 2>/dev/null || true
 docker rm portfolio-app 2>/dev/null || true
 
 # Run docker instance
-docker run -d \
+docker run -it \
     --name portfolio-app \
     --restart unless-stopped \
     -v $(pwd):/app \
     -p 3000:3000 \
     -w /app \
-    ruby:3.4.3
-
-# Install and update dependencies
-gem install bundler
-bundle install
-# Run database setup
-rails db:create RAILS_ENV=production
-rails db:migrate RAILS_ENV=production
-rails db:seed RAILS_ENV=production
-rails assets:precompile RAILS_ENV=production
-# Start the Rails server
-rails server -b 0.0.0.0 -p 3000 -e production
+    ruby:3.4.3 bash -i -c "
+        # Install and update dependencies
+        gem install bundler
+        bundle install
+        # Run database setup
+        rails db:create RAILS_ENV=production
+        rails db:migrate RAILS_ENV=production
+        rails db:seed RAILS_ENV=production
+        rails assets:precompile RAILS_ENV=production
+        # Start the Rails server
+        rails server -b 0.0.0.0 -p 3000 -e production
+   "
 
 # Show container status
 docker ps
