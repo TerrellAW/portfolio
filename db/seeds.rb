9cc416b602e54434f10078ceda78c7d95754dcfd
TerrellAW <terraw1999@proton.me>
2025-10-05 12:12:27 -0600

    Fix: Seeds will update database
    Seeds would not update existing database entries, do the featured list would not be updated. Now when I add the is_featured bool to a project that should properly change whether its displayed on index.

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,8 @@ web_dev_tag = Tag.find_or_create_by!(name: "Web Development")
 monogame_tag = Tag.find_or_create_by!(name: "MonoGame")
 
 # Create Mood Hops
-mood_hops = Project.find_or_create_by!(name: "Mood Hops") do |project|
+mood_hops = Project.find_or_create_by!(name: "Mood Hops")
+mood_hops.update!(
   project.thumbnail_desc = "A Complex React Native Project - A simple mobile platformer game with levels based on emotions."
   project.desc = "Our team jumped at the opportunity to create a game for our Mobile App Development class' final project. We demonstrated our skills with React Native by doing something no one has done before, creating a platformer game using the tools given to us by React Native. Tools used to render responsive web apps became a game engine where we brought our ideas to life. Mood Hops uses Firebase to handle user accounts and stores save data for level highscores to the device it's played on. With the technologies we are using it would be possible to expand online functionalities by instead saving highscores to the cloud and adding an online leaderboard between users."
   project.img_1_desc = "Login"
@@ -21,13 +22,14 @@ mood_hops = Project.find_or_create_by!(name: "Mood Hops") do |project|
   project.img_3_desc = "and Play!"
   project.color = "#ec4899"
   project.is_featured = true
-end
+)
 
 # Associate tags with Mood Hops
 mood_hops.tags = [ react_native_tag, mobile_tag, game_development_tag ] if mood_hops.tags.empty?
 
 # Create Discord Chatbot
-discord_chatbot = Project.find_or_create_by!(name: "Discord Chatbot") do |project|
+discord_chatbot = Project.find_or_create_by!(name: "Discord Chatbot")
+discord_chatbot.update!(
   project.thumbnail_desc = "A Local AI Powered Chatbot - Uses Discord API to allow AI to communicate with users."
   project.desc = "This program demonstrates the limitless possibilities integrating APIs create. Using the LM Studio SDK to send commands from a codebase to a locally run LLM alongside discord.js, which integrates with the Discord API, has allowed me to create an AI agent which can communicate with Discord users. Using the power of an LLM allows one to create a chatbot with its own personality and goals to bring some fun to a Discord server. The chatbot also has a growing list of commands which can be run by Discord users to gain information from other APIs or generate embedded and interactive responses."
   project.github_link = "https://github.com/TerrellAW/Discord-Chatbot"
@@ -36,13 +38,14 @@ discord_chatbot = Project.find_or_create_by!(name: "Discord Chatbot") do |projec
   project.img_3_desc = "Can integrate with MMO game APIs."
   project.color = "#FF4B33"
   project.is_featured = true
-end
+)
 
 # Associate tags with Discord Chatbot
 discord_chatbot.tags = [ ai_tag, discord_api_tag ] if discord_chatbot.tags.empty?
 
 # Create Allergy Free Recipes
-allergy_free_recipes = Project.find_or_create_by!(name: "Allergy Free Recipes") do |project|
+allergy_free_recipes = Project.find_or_create_by!(name: "Allergy Free Recipes")
+allergy_free_recipes.update!(
   project.thumbnail_desc = "A web app for searching and sharing recipes - Uses a custom search and filtering engine to allow users to find allergy free recipes."
   project.desc = "Using the Next.js technology stack, I built this web app around a search funtion with additional filtration capabilities to filter out allergens for users looking for recipes. The search function will filter for recipes based on the user's search terms, and hide any recipes that contain the ingredients the user is allergic to."
   project.link = "https://allergy-free-recipes.vercel.app/"
@@ -51,13 +54,14 @@ allergy_free_recipes = Project.find_or_create_by!(name: "Allergy Free Recipes") 
   project.img_2_desc = "Log in with Google!"
   project.img_3_desc = "Add your own recipes!"
   project.color = "#4766CD"
-end
+)
 
 # Associate tags with Allergy Free Recipes
 allergy_free_recipes.tags = [ react_tag, nextjs_tag, wev_dev_tag ] if allergy_free_recipes.tags.empty?
 
 # Create C# Loving Bones PONG
-pong_game = Project.find_or_create_by!(name: "C# Loving Bones PONG") do |project|
+pong_game = Project.find_or_create_by!(name: "C# Loving Bones PONG")
+pong_game.update!(
   project.thumbnail_desc = "A PONG game with a skeletal aesthetic - Uses MonoGame and C# to recreate a classic game for learning purposes."
   project.desc = "I built this game using nothing but the MonoGame framework to learn how videogames work. This game can be improved, but it is a fully functional videogame with a relatively custom game engine, since I didn't use a pre-made one. I learned alot about how games work and this not only got my foot in the door for game development, but also made me a better programmer."
   project.github_link = "https://github.com/TerrellAW/CSharp_Loving_Bones_PONG"
@@ -66,7 +70,7 @@ pong_game = Project.find_or_create_by!(name: "C# Loving Bones PONG") do |project
   project.img_3_desc = "Win or lose!"
   project.color = "#FFFFFF"
   project.is_featured = true
-end
+)
 
 # Associate tags with C# Loving Bones PONG
 pong_game.tags = [ game_development_tag, monogame_tag ] if pong_game.tags.empty?
